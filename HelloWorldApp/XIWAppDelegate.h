b96c7bc0322933d4d41a97abdfbdca0a235dbba6
User <user@dhcp0591.hrn.resnet.group.upenn.edu>
2013-09-06 17:57:55 -0400

    Initial Commit

---
+++ HelloWorldApp/XIWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XIWAppDelegate.h
+//  HelloWorldApp
+//
+//  Created by User on 9/6/13.
+//  Copyright (c) 2013 Xinran Wabg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XIWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorldApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorldApp
+//
+//  Created by User on 9/6/13.
+//  Copyright (c) 2013 Xinran Wabg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XIWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XIWAppDelegate class]));
+    }
+}
